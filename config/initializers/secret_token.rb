cc5987568900799adf72062c1319ee47b9c6909b
Ethan <basscase@hotmail.co.uk>
2014-06-22 11:22:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ULKCMap::Application.config.secret_token = 'de340c0890d9fdf50de61742c8d1744961f80666d40d6bf5397cb92d3aee1fde52821acb8703da7aedafd68221233e5ef5794dd08c66dc51c16ac73796789aa0'
